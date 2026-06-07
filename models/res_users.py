455423fac77988639e7e813441daef2d3c06ccea
Ali Hassan <156808621+devhassan17@users.noreply.github.com>
2026-06-07 23:29:34 +0500

    refactor: redefine mobile field as a standalone Char field in res.users

--- models/res_users.py
+++ models/res_users.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    mobile = fields.Char(related='partner_id.mobile', inherited=True, readonly=False)
+    mobile = fields.Char(string='Mobile')
